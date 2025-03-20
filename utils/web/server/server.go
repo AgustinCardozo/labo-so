be3fce84f6559a2e988bc98875735eee25bf5afb
AgustinCardozo <agus.cardozo96@gmail.com>
2025-03-20 13:17:00 -0300

    feat: soporte para retornar json
    se agrega nueva funcion para convertir un objecto en json en Server

--- utils/web/server/server.go
+++ utils/web/server/server.go
@@ -1,20 +1,31 @@
 package server
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"strconv"
 )
 
 func InitServer(puerto int) error {
-	// Declara el puerto
 	port := ":" + strconv.Itoa(puerto)
 
-	// Escucha y sirve con la información de config.json
 	err := http.ListenAndServe(port, nil)
 	if err != nil {
 		fmt.Println("Error al escuchar en el puerto " + port)
 		fmt.Println(err)
 	}
 	return err
+}
+
+func SendJSONResponse(writer http.ResponseWriter, data interface{}) {
+	response, err := json.Marshal(data)
+	if err != nil {
+		http.Error(writer, "Error al convertir datos a JSON", http.StatusInternalServerError)
+		return
+	}
+
+	writer.Header().Set("Content-Type", "application/json")
+	writer.WriteHeader(http.StatusOK)
+	writer.Write(response)
 }
