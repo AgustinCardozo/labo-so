8300fa444ed1cde56551a84e4b12330e0049a4b6
AgustinCardozo <agus.cardozo96@gmail.com>
2025-03-29 23:54:43 -0300

    feat: soporte de log en cpu

--- cpu/cpu.go
+++ cpu/cpu.go
@@ -2,10 +2,24 @@ package main
 
 import (
 	"fmt"
+	"github.com/AgustinCardozo/labo-so/cpu/models/global"
+	"github.com/AgustinCardozo/labo-so/utils/config"
 	"github.com/AgustinCardozo/labo-so/utils/greetings"
+	"github.com/AgustinCardozo/labo-so/utils/log"
+	"log/slog"
+)
+
+const (
+	ConfigPath = "./config/cpu.json"
+	LogPath    = "./logs/cpu.log"
 )
 
 func main() {
 	message := greetings.Hello("Burak")
 	fmt.Println(message)
+
+	config.InitConfig(ConfigPath, &global.ConfigCpu)
+	log.InitLogger(LogPath, global.ConfigCpu.LogLevel)
+
+	slog.Debug("test")
 }
---
+++ cpu/models/global/global.go
@@ -0,0 +1,12 @@
+package global
+
+type CpuConfig struct {
+	IpMemory   string `json:"ip_memory"`
+	PortMemory int    `json:"port_memory"`
+	IpKernel   string `json:"ip_kernel"`
+	PortKernel int    `json:"port_kernel"`
+	Port       int    `json:"port"`
+	LogLevel   string `json:"log_level"`
+}
+
+var ConfigCpu *CpuConfig
